9dd10b10e21d2d5aa7afb879e73e7efff59758e7
Tomoya Tanjo <ttanjo@gmail.com>
2014-02-28 01:28:19 +0900

    Add parse

--- matrix2ut.d
+++ matrix2ut.d
@@ -1,6 +1,51 @@
 module matrix2ut;
 
-import std.array:     Appender;
+import std.algorithm: until;
+import std.array:     Appender, array, empty;
+import std.range:     drop, sequence, take, walkLength;
+
+enum naturals = sequence!"a[0]+n"(1);
+static assert(naturals[0] == 1);
+
+auto parse(string[] header) pure @safe nothrow
+{
+    size_t[][string] ret;
+    immutable headerList = [
+        "func_name", "temp_arg", "in", "in_exp",
+        "return", "out_exp", "result",
+        ];
+    auto idx = naturals;
+    foreach(h; ["func_name", "temp_arg", "in", "in_exp",
+                "return", "out_exp", "result"])
+    {
+        auto tmp = header.until(h);
+        auto len = tmp.walkLength;
+        ret[h] = idx.take(len).array;
+        idx.drop(len);
+        header = header.drop(len);
+    }
+    return ret;
+}
+
+pure @safe nothrow unittest
+{
+    auto header = [
+        "func_name",
+        "temp_arg", "temp_arg",
+        "in",
+        "in_exp", "in_exp", "in_exp",
+        "return",
+        "out_exp", "out_exp",
+        "result"];
+    auto ret = header.parse();
+    assert(ret["func_name"] = [0]);
+    assert(ret["temp_arg"]  = [1, 2]);
+    assert(ret["in"]        = [3]);
+    assert(ret["in_exp"]    = [4, 5, 6]);
+    assert(ret["return"]    = [7]);
+    assert(ret["out_exp"]   = [8, 9]);
+    assert(ret["result"]    = [10]);
+}
 
 string generateUnittest(in string[][] matrix) pure @safe
 in
@@ -22,7 +67,7 @@ body
     foreach (line; matrix)
     {
         string funcName = line[0];
-        string result line[$-1];
+        // string result line[$-1];
 
         
     }
