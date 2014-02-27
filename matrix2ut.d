2cc4dc4de57929993c73f671c15996b6f09ba028
SHOO <zan77137@nifty.com>
2014-02-28 00:26:39 +0900

    Add to2DArray

--- matrix2ut.d
+++ matrix2ut.d
@@ -29,3 +29,23 @@ body
 
     return app.data.join();
 }
+
+/*******************************************************************************
+ * Read to convert csv to strings of 2D array.
+ */
+string[][] to2DArray(string csvdata)
+{
+	import std.array, std.csv;
+	auto app = appender!(string[][])();
+	foreach (data; csvReader!string(csvdata))
+	{
+		app.put(array(data).dup);
+	}
+	return app.data;
+}
+
+unittest
+{
+	enum csvdata = "a,b,c\nd,e,f\ng,h,i";
+	static assert(csvdata.to2DArray() == [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]]);
+}
